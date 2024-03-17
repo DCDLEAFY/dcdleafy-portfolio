f877dd61ea832e94cd36c9e26e42a418e291b379
dcdleafy <allgaier.alif@gmail.com>
2024-03-17 20:51:43 +0000

    imported navbar component to app

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PortfolioNavbarComponent } from './core/components/portfolio-navbar/portfolio-navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    PortfolioNavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
