772a06277892b7a92d680a0656ccc4e2c80c2064
dcdleafy <allgaier.alif@gmail.com>
2024-03-18 22:01:37 +0000

    Moved nav information back to app

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
 import { FolioContainerComponent } from './core/components/folio-container/folio-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    RouterLink,
     FolioContainerComponent
   ],
   templateUrl: './app.component.html',
