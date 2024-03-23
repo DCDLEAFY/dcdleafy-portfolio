beb9023f3530f90e30fb0abf7a1c9f92c80d0363
dcdleafy <allgaier.alif@gmail.com>
2024-03-23 22:11:18 +0000

    created basic card component, added custom font and background color

---
+++ portfolio/src/app/core/components/portfolio-card-content/portfolio-card-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-card-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-card-content.component.html',
+  styleUrl: './portfolio-card-content.component.scss'
+})
+export class PortfolioCardContentComponent {
+
+}
