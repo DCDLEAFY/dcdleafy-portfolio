ea718aa17f1338c742ea182c144cb0354f6c0c2b
dcdleafy <allgaier.alif@gmail.com>
2024-03-24 22:09:20 +0000

    added span header css to global scss file, and created work experience files. updated css globally

---
+++ portfolio/src/app/core/components/content/work-experience-content/work-experience-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-experience-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './work-experience-content.component.html',
+  styleUrl: './work-experience-content.component.scss'
+})
+export class WorkExperienceContentComponent {
+
+}
